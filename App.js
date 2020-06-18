ceeec7324e2ad963e4a60833c7e661ce1319e959
Mills McIlroy <37815163+millsmcilroy@users.noreply.github.com>
2020-06-17 23:25:40 -0600

    41 complete

--- App.js
+++ App.js
@@ -1,23 +1,25 @@
-import { createAppContainer } from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack'
-import HomeScreen from './src/screens/HomeScreen'
-import ComponentsScreen from './src/screens/ComponentsScreen'
-import ListScreen from './src/screens/ListScreen'
-import ImageScreen from './src/screens/ImageScreen'
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import ComponentsScreen from "./src/screens/ComponentsScreen";
+import ListScreen from "./src/screens/ListScreen";
+import ImageScreen from "./src/screens/ImageScreen";
+import CounterScreen from "./src/screens/CounterScreen";
 
 const navigator = createStackNavigator(
   {
     Components: ComponentsScreen,
+    Counter: CounterScreen,
     Home: HomeScreen,
     Image: ImageScreen,
-    List: ListScreen
+    List: ListScreen,
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: "Home",
     defaultNavigationOptions: {
-      title: 'App'
-    }
+      title: "App",
+    },
   }
-)
+);
 
-export default createAppContainer(navigator)
+export default createAppContainer(navigator);
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,28 +3,20 @@ import { Button, Text, StyleSheet, TouchableOpacity, View } from 'react-native'
 
 const HomeScreen = ({ navigation }) => {
   return (
-      <View>
-        <Text style={styles.text}>App Screen</Text>
-        <Button
-            onPress={() => navigation.navigate('Components')}
-            title="Go to Components Demo"
-        />
-        <Button
-          onPress={() => navigation.navigate('List')}
-          title="Go to List Demo"
-        />
-          <Button
-              onPress={() => navigation.navigate('Image')}
-              title="Go to Image Demo"
-          />
-      </View>
+    <View>
+      <Text style={styles.text}>App Screen</Text>
+      <Button onPress={() => navigation.navigate('Components')} title='Go to Components Demo' />
+      <Button onPress={() => navigation.navigate('List')} title='Go to List Demo' />
+      <Button onPress={() => navigation.navigate('Image')} title='Go to Image Demo' />
+      <Button onPress={() => navigation.navigate('Counter')} title='Go to Counter Demo' />
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 })
 
 export default HomeScreen
